f4cd302f91ebe34e7f6ee95ae6d058cf7306101e
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-18 17:43:37 -0300

    fix hero banner image

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -8,19 +8,31 @@ const HeroBanner = ({ heroBanner }) => {
       <div>
         <p className="text-xl">{heroBanner.smallText}</p>
         <h3 className="text-6xl mt-1">{heroBanner.midText}</h3>
-        <h1 className="text-white text-9xl -ml-5 uppercase">{heroBanner.largeText}</h1>
+        <h1 className="text-white text-9xl -ml-5 uppercase">
+          {heroBanner.largeText}
+        </h1>
         <img
           src={urlFor(heroBanner.image)}
           alt="headphones"
-          className="hero-banner-image"
+          className="hero-banner-image relative left-60 -top-40"
         />
         <div>
           <Link href={`/product/${heroBanner.product}`}>
-            <button className="rounded-2xl px-2.5 py-4 bg-indigo-600 text-white mt-10 text-lg font-medium cursor-pointer" style={{zIndex: 10000}} type="button">{heroBanner.buttonText}</button>
+            <button
+              className="rounded-2xl px-2.5 py-4 bg-indigo-600 text-white mt-10 text-lg font-medium cursor-pointer"
+              style={{ zIndex: 10000 }}
+              type="button"
+            >
+              {heroBanner.buttonText}
+            </button>
           </Link>
           <div className="absolute right-3 bottom-1 w-300 flex flex-col">
-            <h5 className="mb-3 font-bold text-base self-end text-indigo-600">Description</h5>
-            <p className="font-base text-end text-gray-600">{heroBanner.desc}</p>
+            <h5 className="mb-3 font-bold text-base self-end text-indigo-600">
+              Description
+            </h5>
+            <p className="font-base text-end text-gray-600">
+              {heroBanner.desc}
+            </p>
           </div>
         </div>
       </div>
